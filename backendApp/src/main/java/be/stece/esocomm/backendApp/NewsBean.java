00d151e048104759d8b10b36e835106f04d93968
Stece <stephan.celis@gmail.com>
2014-06-09 12:28:30 +0200

    Create NewsBean.

---
+++ backendApp/src/main/java/be/stece/esocomm/backendApp/NewsBean.java
@@ -0,0 +1,41 @@
+package be.stece.esocomm.backendApp;
+
+/**
+ * Created by Stece on 9/06/2014.
+ */
+public class NewsBean {
+
+    private long id;
+    private String title;
+    private String body;
+
+    public NewsBean(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
